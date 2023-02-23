04412295d56dd884d9a80a08ea39e9db6a191b12
Sergey Drepin <sdrepin@mail.ru>
2023-02-23 10:47:17 +0300

    Do program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿// Проверка на полиндром
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
-string number = 14212;
+string number = Convert.ToString(23432);
 
-
-if (number[0] == number(4) && number[1] == number(3))
+if (number[0] == number[4] && number[1] == number[3])
 {
     Console.WriteLine("Yes");
 }
